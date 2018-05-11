2d06e50d1277fc941f766ed4da1dd7763ca7dff5
Dylan Abrames <abrames.dylan@gmail.com>
2018-05-10 20:25:02 -0700

    seeded pieces to the pieces table.  id's 1 to 6 for each piece type

---
+++ models/pieces.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Piece = sequelize.define('pieces', {
+    name: DataTypes.STRING,
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Piece;
+};
