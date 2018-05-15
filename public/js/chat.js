0950e86365ce0ee6d99057331cf7fa7ff33070db
Dylan Abrames <abrames.dylan@gmail.com>
2018-05-14 19:31:20 -0700

    fuck1

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
   // initializing socket, connection to server
   //var socket = io.connect('https://csc-667-team-f.herokuapp.com:7777');
-  const socket = io.connect('//localhost:3000');
+  const socket = io.connect('http//localhost:3000');
   socket.on('connect', function(data) {
     socket.emit('join', 'Hello server from client');
   });
