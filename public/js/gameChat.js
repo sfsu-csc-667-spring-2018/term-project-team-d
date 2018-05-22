d2901375259a1fdc225d335c01266825bf9bdba1
menaczar <menarondaroad@hotmail.com>
2018-05-21 23:41:34 -0700

    private chat rooms

--- public/js/gameChat.js
+++ public/js/gameChat.js
@@ -1,7 +1,10 @@
   // initializing socket, connection to server
-  const gameChatSocket = io('/gameChatSocket');
+  var room = document.getElementsByClassName("gameid")[0].getAttribute('gameid');
+  console.log(room);
+  const gameChatSocket = io.connect('http://localhost:3000');
   gameChatSocket.on('connect', function(data) {
-    gameChatSocket.emit('join', 'Hello server from game chat');
+    gameChatSocket.emit('room', room);
+    //gameChatSocket.emit('join', 'Hello server from game chat: ' + room);
   });
 
   // listener for 'thread' event, which updates messages
@@ -18,6 +21,7 @@
       var username = $('#user').text();
       var full_message = username + ': ' + message;
       gameChatSocket.emit('messages', full_message);
+      //gameChatSocket.emit('room', room);
       this.reset();}
     return false;
  });
